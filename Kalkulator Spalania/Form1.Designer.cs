f5e640e20851d4ee66162b63a13dd5c8848198c7
SzJagosz <sz.jagosz@gmail.com>
2020-09-24 15:32:12 +0200

    1.0.0

---
+++ Kalkulator Spalania/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Kalkulator_Spalania
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(204, 112);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(157, 30);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Image = global::Kalkulator_Spalania.Properties.Resources.OBLICZ;
+            this.button1.Location = new System.Drawing.Point(45, 180);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 30);
+            this.button1.TabIndex = 3;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox2.Location = new System.Drawing.Point(204, 67);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(157, 30);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(39)))), ((int)(((byte)(52)))));
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label4.Font = new System.Drawing.Font("Courier New", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(166)))), ((int)(((byte)(82)))));
+            this.label4.Location = new System.Drawing.Point(375, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(306, 42);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "--,- L/100km";
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Image = global::Kalkulator_Spalania.Properties.Resources.KASUJ;
+            this.button2.Location = new System.Drawing.Point(218, 180);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 30);
+            this.button2.TabIndex = 8;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Image = global::Kalkulator_Spalania.Properties.Resources.X;
+            this.button3.Location = new System.Drawing.Point(653, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(37, 35);
+            this.button3.TabIndex = 9;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Kalkulator_Spalania.Properties.Resources.SPALANIE_;
+            this.ClientSize = new System.Drawing.Size(686, 239);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Kalkulator Spalania";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Kalkulator Spalania/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kalkulator_Spalania
+{
+    public partial class Form1 : Form
+
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Point lastPoint;
+        private void button2_Click(object sender, EventArgs e) // Przycisk Kasuj został wciśnięty
+        {
+
+            if(textBox1.TextLength + textBox2.TextLength == 0) // Jeżeli pola textBox1 i textBox2 są puste to:
+            {
+                MessageBox.Show("Obecnie pola są już puste", "Kasuj Dane", MessageBoxButtons.OK, MessageBoxIcon.Warning); // Zwróc MessageBox z informacją, że pola są już puste
+                
+            }
+
+            else
+            {
+                textBox1.Text = "";                             //
+                textBox2.Text = "";                             // W przeciwnym wypadku wyczyść oba pola i wynik
+                label4.Text = "--,- L/100km"; //
+            }
+        }
+        private void button1_Click(object sender, EventArgs e) // Przycisk Oblicz został wciśnięty
+        {
+
+                if (textBox2.Text.Contains(".") || textBox1.Text.Contains("."))                                   // Sprawdź czy w polu textBox1 albo textBox2 znajduje się kropka
+                {
+                    MessageBox.Show("Zamiast kropki wpisz przecinek", "Oblicz", MessageBoxButtons.OK, MessageBoxIcon.Error);  //
+                    textBox1.Text = "";                                                                                       // Jeżeli tak, to zwróc MessageBox z informacją
+                    textBox2.Text = "";                                                                                       // o zamianie kropki na przecinek i wyczyść oba pola i wynik
+                    label4.Text = "--,- L/100km";                                                           //
+                }
+
+                else                                                                                                // Jeżeli w obu polach nie wystąpiły kropki to:
+                {
+
+                    if (textBox1.TextLength > 0 & textBox2.TextLength > 0)                                          // Sprawdź czy w polu textBox1 albo textBox2 znajduja sie dane
+                    {
+                        decimal calc = Convert.ToDecimal(textBox2.Text) / (Convert.ToDecimal(textBox1.Text) / 100); // Obliczenie spalania do zmiennej calc
+                        label4.Text = Convert.ToString(calc).Substring(0, 4) + " L/100km";  // Zamień tekst label4 na wartosc calc przekonwertowaną na string i zaokrąglij
+                    }
+
+                    else                                                                                            // Jeżeli w obu polach nie wystąpiły dane to
+                    {                                                                                               // Wyświetl messageBox z informacja o wprowadzeniu danych
+                        MessageBox.Show("Wprowadź dane!", "Oblicz", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }    
+            }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)  // Sprawdź jakie przyciski wciśnięto w textBox1
+        {
+            if (!char.IsDigit(e.KeyChar) && (e.KeyChar != ',') && !char.IsControl(e.KeyChar))             // Jeśli jest to licza / przecinek, to pozwól ją wpisać
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e) // Sprawdź jakie przyciski wciśnięto w textBox2
+        {
+            if (!char.IsDigit(e.KeyChar) && (e.KeyChar != ',') && !char.IsControl(e.KeyChar))               // Jeśli jest to licza / przecinek, to pozwól ją wpisać
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+    }
+    }
+// 23.09.2020, Szymon Jagosz
+
